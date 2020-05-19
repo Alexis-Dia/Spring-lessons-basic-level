c3142ca1f585ee238c49319c5eb21451de896cb9
Alexis-Dk <alexeydruzik@inbox.ru>
2020-05-19 12:32:21 +0300

    Added description about differences between @Autowired and injection using constructor.

--- lesson4-raven-starter/src/main/java/ironbank/RavenListener.java
+++ lesson4-raven-starter/src/main/java/ironbank/RavenListener.java
@@ -8,6 +8,11 @@ import org.springframework.context.event.ContextRefreshedEvent;
  */
 public class RavenListener implements ApplicationListener<ContextRefreshedEvent> {
 
+    /** This example shows, that in IronConfiguration-class when we use RavenListener with @Bean,
+    Spring automatically set value of ravenProperties-bean through argument of ravenListener-method
+    Here Juergen Hoeller advices to set @Autowired for better understating it.
+     **/
+    //@Autowired
     private final RavenProperties ravenProperties;
 
     public RavenListener(RavenProperties ravenProperties) {
