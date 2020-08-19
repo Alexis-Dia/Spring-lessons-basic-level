42af68b0a2baa0325d7bbdc649bedc4f9804c48b
Alexis-Dk <alexeydruzik@inbox.ru>
2020-08-19 14:32:18 +0300

    Added second example of configuring beans.

--- lesson4-raven-starter/src/main/java/ironbank/IronConfiguration.java
+++ lesson4-raven-starter/src/main/java/ironbank/IronConfiguration.java
@@ -4,15 +4,37 @@ import ironbank.annotation.ConditionOnProduction;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Alexey Druzik on 5/2/2020
  */
 /* When we are using spring.factories - it's not necessary use @Configuration*/
-//@Configuration
+@Configuration
 @EnableConfigurationProperties(RavenProperties.class)
 public class IronConfiguration {
 
+    /**
+        Update from 19.08.2020: У Spring есть такая фишка, что если в контексте есть бин(не важно как созданный - напрямую
+        с помощью аннотации @Bean или завтоваринный @Autowire), то помимо изания над полем @Bean, мы можем эту аннотацию
+        юзать и над методом, в этом случае есть фишки:
+            1. Мы можем юзать в качестве аргументов этого метода другие бины, которые есть в контексте
+            2. Мы можем теле метода изменять эти бины, таким образом делая настройку созданных бинов в Spring-контейнере.
+     **/
+
+    /**
+     * Update from 19.08.2020 - It's Eugene Borisov's way:
+     */
+    @Bean
+    @ConditionOnProduction
+    @ConditionalOnProperty("raven.destination")
+    public RavenListener ravenListener() {
+        return new RavenListener();
+    }
+
+    /**
+     * Update from 19.08.2020 - It's official Spring way:
+     */
     @Bean
     @ConditionOnProduction
     @ConditionalOnProperty("raven.destination")
--- lesson4-raven-starter/src/main/java/ironbank/RavenListener.java
+++ lesson4-raven-starter/src/main/java/ironbank/RavenListener.java
@@ -1,20 +1,37 @@
 package ironbank;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 /**
+ * This example shows, that in IronConfiguration-class when we use RavenListener with @Bean,
+ * Spring automatically set value of ravenProperties-bean through argument of ravenListener-method
+ * Here Juergen Hoeller advices to set @Autowired for better understating it.
+ * Update from 19.08.2020 - аннотация @Autowired может работать над полем, по крайней мере в двух режимах:
+ *     1. Когда есть интерфейс и есть имплементация(спрингу сказано в каких папках искть при старте контекста)
+ *     2. Когда мы в юзаем аннотацию @EnableConfigurationProperties(RavenProperties.class), т е мы потом в других классах можем юзать:
+ *         @Autowired RavenProperties ravenProperties
  * @author Alexey Druzik on 5/2/2020
  */
 public class RavenListener implements ApplicationListener<ContextRefreshedEvent> {
 
-    /** This example shows, that in IronConfiguration-class when we use RavenListener with @Bean,
-    Spring automatically set value of ravenProperties-bean through argument of ravenListener-method
-    Here Juergen Hoeller advices to set @Autowired for better understating it.
-     **/
-    //@Autowired
-    private final RavenProperties ravenProperties;
+    /**
+     * Update from 19.08.2020 - It's Eugene Borisov's way.
+     *     В этом случае бин RavenListener будет создан с помощью рефлексии. Т е Spring сам вызовет данный пустой конструктор:
+     */
+    public RavenListener() {
+    }
+    @Autowired
+    private RavenProperties ravenProperties;
 
+    /**
+     * Update from 19.08.2020 - It's official Spring way.
+     *     В этом случае бин RavenListener будет создан не с помощью рефлексии, а вручную нами. Так же в этом случае в
+     *     документации рекомендуется ставить аннотацию @Autowired, что бы было понятнее. Так же в этом способе нужно
+     *     юзать филд - private final RavenProperties ravenProperties;
+     */
+    @Autowired
     public RavenListener(RavenProperties ravenProperties) {
         this.ravenProperties = ravenProperties;
     }
